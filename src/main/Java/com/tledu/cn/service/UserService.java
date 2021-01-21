10b973726f105d3e790e11c68a50a9a26e7512bc
17uhz <1468414452@qq.com>
2021-01-21 09:25:06 +0800

    用户模块提交

---
+++ src/main/Java/com/tledu/cn/service/UserService.java
@@ -0,0 +1,21 @@
+package com.tledu.cn.service;
+
+import com.tledu.cn.pojo.User;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * Author:17
+ * Date:2021-01-20 13:04
+ * Description:<描述>
+ */
+public interface UserService {
+    int registerUser(User user);
+
+    int userLogin(HttpServletRequest request, User user);
+    int changePwd(User user);
+
+    Map<String,Boolean> uploadImage(HttpServletRequest request);
+
+}
